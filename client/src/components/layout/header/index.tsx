128da8a4ddc1a07f8d6c05a1e5d9b528f9236049
fazzy12 <kaluifeanyi00@gmail.com>
2023-06-10 19:51:05 -0700

    refactor: Add comments and remove unused code in Header component

--- client/src/components/layout/header/index.tsx
+++ client/src/components/layout/header/index.tsx
@@ -12,15 +12,22 @@ import {
 
 // import { ColorModeContext } from 'contexts';
 
+// Header component
 export const Header: React.FC = () => {
   // const { mode, setMode } = useContext(ColorModeContext);
 
+  // Retrieve user identity data
   const { data: user } = useGetIdentity();
   const showUserInfo = user && (user.name || user.avatar);
 
   return (
-    // CHANGE: header color & elevation
-    <AppBar color="default" position="sticky" elevation={0} sx={{ background: '#FCFCFC' }}>
+    // CHANGE: Customize header appearance
+    <AppBar
+      color="default"
+      position="sticky"
+      elevation={0}
+      sx={{ background: '#FCFCFC' }}
+    >
       <Toolbar>
         <Stack
           direction="row"
@@ -36,14 +43,25 @@ export const Header: React.FC = () => {
             {mode === 'dark' ? <LightModeOutlined /> : <DarkModeOutlined />}
           </IconButton> */}
           {showUserInfo && (
-            <Stack direction="row" gap="12px" alignItems="center" justifyContent="center">
+            <Stack
+              direction="row"
+              gap="12px"
+              alignItems="center"
+              justifyContent="center"
+            >
               {user.avatar && <Avatar src={user?.avatar} alt={user?.name} />}
 
               {user.name && (
                 // CHANGE: Display user info on the header
                 <Stack direction="column">
-                  <Typography sx={{ fontSize: 14, fontWeight: 600, color: '#11142D' }}>{user?.name}</Typography>
-                  <Typography sx={{ fontSize: 12, color: '#808191' }}>{user?.email}</Typography>
+                  <Typography
+                    sx={{ fontSize: 14, fontWeight: 600, color: '#11142D' }}
+                  >
+                    {user?.name}
+                  </Typography>
+                  <Typography sx={{ fontSize: 12, color: '#808191' }}>
+                    {user?.email}
+                  </Typography>
                 </Stack>
               )}
             </Stack>
