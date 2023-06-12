59effa40b9affdee5f1f70f05d283cd12bcc5135
fazzy12 <kaluifeanyi00@gmail.com>
2023-06-12 10:33:13 -0700

    Added exports for all components and added comments for clarity

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -9,13 +9,13 @@ import PropertyDetails from './property-details';
 import EditProperty from './edit-property';
 
 export {
-  AgentProfile,
-  Agents,
-  AllProperties,
-  CreateProperty,
-  Home,
-  Login,
-  MyProfile,
-  PropertyDetails,
-  EditProperty,
+  AgentProfile, // Component for displaying agent profile
+  Agents, // Component for displaying a list of agents
+  AllProperties, // Component for displaying all properties
+  CreateProperty, // Component for creating a new property
+  Home, // Component for the home page
+  Login, // Component for the login page
+  MyProfile, // Component for displaying the user's profile
+  PropertyDetails, // Component for displaying property details
+  EditProperty, // Component for editing a property
 };
